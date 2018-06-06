79592dfc96fe878984b6dcc5eacdd71e9bbfaec5
freesky12138 <1774549483@qq.com>
2018-06-06 19:17:43 +0800

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * @author hyp 1774549483@qq.com
+ * @version v1.0
+ * @Title: creat my AlgorithmRepertory
+ * @description
+ * @date 2018/6/6 19:11
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("hello world");
+    }
+}
