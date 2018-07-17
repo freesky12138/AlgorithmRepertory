f7b909181a1e5627dd6eaa19d1a07e234d8323c7
freesky12138 <1774549483@qq.com>
2018-07-17 17:44:00 +0800

    模运算

---
+++ src/main/java/math/Mod.java
@@ -0,0 +1,17 @@
+package math;
+
+/**
+ * @author hyp 1774549483@qq.com
+ * @version v1.0
+ * @Title:math
+ * @description 模运算
+ * @date 2018/7/4 18:54
+≡（恒等于）如果 a≡c(mod m) 并且 b≡c（mod m） （b对m取余恒等于c对M取余，  b和c之间相差n个m）
+那么就有
+    a+b≡c+d（mod m）
+    a-b≡c-d（mod m）
+    a*b≡c*d（mod m）
+ */
+public class Mod {
+
+}
