754122e95cd760b831e5c216a68099bf4ddc05fd
freesky12138 <1774549483@qq.com>
2018-10-12 16:32:52 +0800

    thread

--- src/main/java/AmazingJava/HighConcurrency/ThreadGroup/package-info.java
+++ src/main/java/AmazingJava/HighConcurrency/ThreadGroup/package-info.java
@@ -2,7 +2,7 @@
  * @Title:AmazingJava.HighConcurrency.ThreadGroup
  * @description
  * @author hyp 1774549483@qq.com
- * ThreadGroup
+ *  ThreadGroup 线程组合线程一样，都存在父子关系
  *
  * @date 2018/10/12 14:57
  * @version v1.0
