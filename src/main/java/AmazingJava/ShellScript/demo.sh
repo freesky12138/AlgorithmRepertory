49387efc481666adabbd9e4e079bff9c3418f418
freesky12138 <1774549483@qq.com>
2018-10-29 23:51:49 +0800

    commit demo.sh

---
+++ src/main/java/AmazingJava/ShellScript/demo.sh
@@ -0,0 +1,35 @@
+echo this is a test
+echo 'this is "abc" test'
+
+echo $HOME
+HOME='/home/huppert'
+
+echo $HOME
+
+testing=$(date)
+echo "this is time" $testing
+
+date > testfile
+
+#rpm -qa | sort | more
+
+var1=123
+var1=$(expr $var1 + $var1)
+var1=$(expr $var1 \* 2)
+echo $var1
+
+var1=$[ $var1 / $var1]
+echo $var1
+
+for(( i=1;i<5;i++))
+do
+	if [ $i -gt 3 ]
+	then		continue
+	elif [ $i -lt 2 ]
+	then		echo -n  "i $i"
+	else
+		echo "$i"
+	fi
+done
+
+exit
