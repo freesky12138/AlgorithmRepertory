4dae97bced27fddb50a77bb8a6bef95e4efc5cd3
freesky12138 <1774549483@qq.com>
2018-11-02 10:32:26 +0800

    thread

--- src/main/java/AmazingJava/HighConcurrency/DeepUnderstandThread/ParentThread.java
+++ src/main/java/AmazingJava/HighConcurrency/DeepUnderstandThread/ParentThread.java
@@ -10,6 +10,7 @@ package AmazingJava.HighConcurrency.DeepUnderstandThread;
  */
 public class ParentThread {
     public static void main(String[] args){
+        System.out.println(Thread.currentThread().getName());
         new Thread(new Runnable() {
             @Override
             public void run() {
---
+++ src/main/java/MainTest.java
@@ -0,0 +1,12 @@
+import java.util.stream.IntStream;
+
+/**
+ * @author hyp 1774549483@qq.com
+ * @version v1.0
+ * @Title:tree
+ * @description
+ * @date 2018/11/2 10:23
+ */
+public class MainTest {
+
+}
