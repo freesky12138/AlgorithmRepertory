8f6f62132e68fa89f7255942d0909d08445ff265
freesky12138 <1774549483@qq.com>
2018-11-07 17:58:22 +0800

    thread

---
+++ src/main/java/DataFac.java
@@ -0,0 +1,22 @@
+/**
+ * @author hyp 1774549483@qq.com
+ * @version v1.0
+ * @Title:PACKAGE_NAME
+ * @description 数据工厂类
+ * @date 2018/11/7 17:48
+ */
+public class DataFac {
+    private String getSomeString(int length) {
+
+        return null;
+    }
+
+    private int[] getSomeInt(int length) {
+        return null;
+    }
+
+    private int[] getSortInt(int length) {
+        return null;
+    }
+
+}
