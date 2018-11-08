7cba37580c298b6ffca626041546ee0f286dbe84
freesky12138 <1774549483@qq.com>
2018-11-08 16:37:03 +0800

    thread

--- src/main/java/DataFac.java
+++ src/main/java/DataFac.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * @author hyp 1774549483@qq.com
  * @version v1.0
@@ -6,17 +9,36 @@
  * @date 2018/11/7 17:48
  */
 public class DataFac {
-    private String getSomeString(int length) {
 
-        return null;
+    private static Random random = new Random();
+
+    public static String getSomeString(int length) {
+        String data = "";
+        for (int i = 0; i < length; i++) {
+            data += "A" + random.nextInt(26);
+        }
+        return data;
     }
 
-    private int[] getSomeInt(int length) {
-        return null;
+    public int[] getSomeInt(int length, int max) {
+        if (length > 100000000)
+            length = 100000000;
+        int[] data = new int[length];
+        for (int i = 0; i < length; i++) {
+            data[i] = random.nextInt(max);
+        }
+        return data;
     }
 
-    private int[] getSortInt(int length) {
-        return null;
+    public int[] getSortInt(int length,int max) {
+        if (length > 100000000)
+            length = 100000000;
+        int[] data = new int[length];
+        for (int i = 0; i < length; i++) {
+            data[i] = random.nextInt(max);
+        }
+        Arrays.sort(data);
+        return data;
     }
 
 }
